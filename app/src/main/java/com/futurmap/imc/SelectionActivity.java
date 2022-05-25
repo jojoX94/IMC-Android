bc0070e360c517d7575839be6d767c3064fda79c
jojoX94 <ramarolacontact@gmail.com>
2022-05-25 09:21:57 +0300

    Main Activity Page done

---
+++ app/src/main/java/com/futurmap/imc/SelectionActivity.java
@@ -0,0 +1,14 @@
+package com.futurmap.imc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
+    }
+}
