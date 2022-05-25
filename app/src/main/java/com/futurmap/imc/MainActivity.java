68c62fcd47bf95fb4a5c430b78ec96d3ec03d2b3
jojoX94 <ramarolacontact@gmail.com>
2022-05-25 16:24:30 +0300

    Selection Portrait layout done

--- app/src/main/java/com/futurmap/imc/MainActivity.java
+++ app/src/main/java/com/futurmap/imc/MainActivity.java
@@ -1,7 +1,11 @@
 package com.futurmap.imc;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void enterHomePage(View view) {
+        Intent intent = new Intent(this, SelectionActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/futurmap/imc/SelectionActivity.java
+++ app/src/main/java/com/futurmap/imc/SelectionActivity.java
@@ -1,14 +1,63 @@
 package com.futurmap.imc;
 
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SelectionActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private SeekBar seekBarTwo;
+    private TextView lbl_height;
+    private TextView lbl_weight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+        seekBar = findViewById(R.id.seekBar);
+        seekBarTwo = findViewById(R.id.seekBarTwo);
+        seekBar.setMax(150);
+        lbl_height = findViewById(R.id.lbl_height);
+        lbl_weight = findViewById(R.id.lbl_weight);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                lbl_height.setText("" + (progress + 150) + " cm");
+                lbl_height.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBarTwo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                lbl_weight.setText("" + (progress + 30) + " kg");
+                lbl_weight.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
