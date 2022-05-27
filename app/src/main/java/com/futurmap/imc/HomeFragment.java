f96a694d0827ff0c982e2eb7bc7cd65e1dcf8892
jojoX94 <ramarolacontact@gmail.com>
2022-05-27 14:58:19 +0300

    Convert Checkboxd to RadioButton selection

--- app/src/main/java/com/futurmap/imc/HomeFragment.java
+++ app/src/main/java/com/futurmap/imc/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,57 +12,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.futurmap.imc.utils.ConstraintRadioGroup;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
     private SeekBar seekBar;
     private SeekBar seekBarTwo;
     private TextView lbl_height;
     private TextView lbl_weight;
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ConstraintRadioGroup constraintRadioGroup;
+    private ImageView img_men;
+    private ImageView img_women;
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -76,6 +45,9 @@ public class HomeFragment extends Fragment {
         seekBar.setMax(150);
         lbl_height = view.findViewById(R.id.lbl_height);
         lbl_weight = view.findViewById(R.id.lbl_weight);
+        constraintRadioGroup = view.findViewById(R.id.radioGroup_gender);
+        img_men = view.findViewById(R.id.img_men);
+        img_women = view.findViewById(R.id.img_women);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -87,12 +59,10 @@ public class HomeFragment extends Fragment {
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
 
@@ -106,12 +76,23 @@ public class HomeFragment extends Fragment {
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
 
+        constraintRadioGroup.SetOnCheckedChangeListener(new ConstraintRadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void OnCheckedChanged(ConstraintRadioGroup rg, int nCheckedId) {
+                if (nCheckedId == R.id.check_men) {
+                    img_men.setVisibility(View.VISIBLE);
+                    img_women.setVisibility(View.GONE);
+                } else {
+                    img_women.setVisibility(View.VISIBLE);
+                    img_men.setVisibility(View.GONE);
+                }
             }
         });
 
